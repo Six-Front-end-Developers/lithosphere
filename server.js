8a2725fdabcc00b5180668e8478ebb53dcc52ca4
Jmmxp <jmmxpdev@gmail.com>
2019-09-07 14:24:07 -0700

    Update server to use express

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 // https://blog.risingstack.com/your-first-node-js-http-server/ tutorial to set up NodeJS server
-const http = require('http')
+const express = require('express')
+const app = express()
 const port = 8000
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
+app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
 
   console.log(`server is listening on ${port}`)
+})
+
+app.get('/', (req, resp) => {
+  // Render webpage
+  console.log('HELLO! This is the root /')
+})
+
+app.get('/earthquakes', (req, resp) => {
+  // Read from database, return it to clients as JSON
+  console.log('HELLO! This is /earthquakes.')
 })
