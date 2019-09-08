17ce626124a3b83de0e73227f01d2ecd45154693
anticobalt <anticobalt@users.noreply.github.com>
2019-09-07 21:53:36 -0600

    Add date and number pickers

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var vueInstance;
+var vueInstance
 
 $(document).ready(function() {
     $(function(){
         $('#world-map').vectorMap({map: 'world_mill'});
+        $('#datepicker-from').datepicker();
+        $('#datepicker-to').datepicker();
+        $(".js-range-slider").ionRangeSlider({
+            type: "double",
+            min: 5,
+            max: 10,
+            step: 0.1
+        });
     });
 
     vueInstance = new Vue({
         el: '#example',
         data: { hello: 'Hello World!' }
     })
+
 })
