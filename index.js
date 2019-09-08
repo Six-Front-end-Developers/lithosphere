f56cef446b2a4bc10f750201dce2290273c75296
kerryli33 <kerry2@ualberta.ca>
2019-09-08 10:21:58 -0600

    made points onto map

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var vueInstance
 
 $(document).ready(function() {
+    let exampleCoord = [[38.30,142.40],'Tokyo, Magnitude: 9, Date: 2011'];
+    let exampleCoord2 = [[50,100.40],'Tokyo, Magnitude: 9, Date: 2011'];
     $(function(){
-        $('#world-map').vectorMap({map: 'world_mill'});
+        $('#world-map').vectorMap({map: 'world_mill',
+        markerStyle: {
+            initial: {
+                fill: '#f52222',
+                stroke: '#2e2e2e'
+            }
+        },
+        markers: [
+            {latLng: exampleCoord[0], name: exampleCoord[1]},
+            {latLng: exampleCoord2[0], name: exampleCoord2[1], style: {fill: '#344533'}},
+            
+        ]
+    });
         $('#datepicker-from').datepicker();
         $('#datepicker-to').datepicker();
         $(".js-range-slider").ionRangeSlider({
